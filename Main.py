8f5698b52bfdeb1d565d7b710f5b2e908dc13eb2
msciborski <michal.sciborski@student.put.poznan.pl>
2019-12-14 16:42:38 +0100

    Add checking in dbpedia

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
 import nltk
+import rdflib
 from nltk.tag import StanfordNERTagger
 from nltk.internals import find_jars_within_path
 from nltk.tokenize import word_tokenize
+
+graph = rdflib.Graph()
 
 
 def initialize_stanford_ner_tagger():
@@ -20,19 +23,43 @@ def tokenize_input(input):
     return word_tokenize(input)
 
 
-def main():
-    input = 'Florence May Harding studied at a school in Sydney, and with Douglas Robert Dundas, but in effect had no formal training in either botany or art.'
+def get_persons_places_organizations(input):
     st = initialize_stanford_ner_tagger()
     tokenized_input = tokenize_input(input)
-    print('Tokenized', tokenized_input)
     classified_input = list(st.tag(tokenized_input))
-    print('Classified input:', classified_input)
-    print(type(classified_input[0]))
-    filtered_classified_input = list(filter(lambda tup: tup[1] == 'LOCATION' or tup[1] == 'PERSON' or tup[1] == 'LOCATION', classified_input))
-    print('Filtered input:', filtered_classified_input)
+    return list(filter(lambda tup: tup[1] == 'LOCATION' or tup[1] == 'PERSON' or tup[1] == 'LOCATION', classified_input))
+
+
+def check_dbpedia(classified_input):
+    result = []
+    person = rdflib.URIRef('http://dbpedia.org/ontology/Person')
+    place = rdflib.URIRef('http://dbpedia.org/ontology/Place')
+    organisation = rdflib.URIRef('http://dbpedia.org/ontology/Organisation')
+
+    for word in classified_input:
+        resource = rdflib.URIRef(f"http://dbpedia.org/resource/Robert")
+        graph.value(resource, rdflib.RDFS.label)
+        graph.load(resource)
+        for subject, predicate, object_ in graph:
+            if subject == resource and object_ in [person, place, organisation]:
+                result.append((word, subject, predicate, object_))
+    return result
+
+
+def main():
+    input = 'Florence May Harding studied at a school in Sydney, and with Douglas Robert Dundas, but in effect had no formal training in either botany or art.'
+    result = get_persons_places_organizations(input)
+    dbpedia_result = check_dbpedia(result)
+    print(dbpedia_result)
+    # st = initialize_stanford_ner_tagger()
+    # tokenized_input = tokenize_input(input)
+    # print('Tokenized', tokenized_input)
+    # classified_input = list(st.tag(tokenized_input))
+    # print('Classified input:', classified_input)
+    # print(type(classified_input[0]))
+    # print('Filtered input:', filtered_classified_input)
+    # filtered_classified_input = list(filter(lambda tup: tup[1] == 'LOCATION' or tup[1] == 'PERSON' or tup[1] == 'LOCATION', classified_input))
 
 
 if __name__ == '__main__':
     main()
-# tagged = st.tag('Rami Eid is studying at Stony Brook University in NY'.split())
-# print(tagged)
